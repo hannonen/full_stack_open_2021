8672b37f74f10d656bac0969f934c2374e46bd07
hannonen <52950689+hannonen@users.noreply.github.com>
2021-04-23 20:28:17 +0300

    Part 1, courseinfo 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -16,13 +16,14 @@ const App = (props) => {
         <Header course={course} />
       </h1>
       <p>
-        <Content content={part1 + " " + exercises1} />
-      </p>
-      <p>
-        <Content content={part2 + " " + exercises2} />
-      </p>
-      <p>
-        <Content content={part3 + " " + exercises3} />
+        <Content
+          part1={part1}
+          exercises1={exercises1}
+          part2={part2}
+          exercises2={exercises2}
+          part3={part3}
+          exercises3={exercises3}
+        />
       </p>
       <p>
         <Total total={"Number of exercises: " + total} />
@@ -33,6 +34,16 @@ const App = (props) => {
 };
 
 const Content = (props) => {
+  return (
+    <div>
+      <Part content={props.part1 + " " + props.exercises1} />
+      <Part content={props.part2 + " " + props.exercises2} />
+      <Part content={props.part3 + " " + props.exercises3} />
+    </div>
+  );
+};
+
+const Part = (props) => {
   return <div>{props.content}</div>;
 };
 
